5764041e0192d0431bd07c640b689e81e0549b3a
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-19 22:47:40 +0100

    preparacion api

--- src/apiRest.js
+++ src/apiRest.js
@@ -1,5 +1,3 @@
 const app = require('./app')
 
-app.listen(app.get("port"), () => {
-    console.log(`Server listen on port ${app.get("port")}`);
-})
+app.listen(process.env.PORT || 3000)
--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "appbooks"
+  host:     process.env.DB_HOST         || "localhost",
+  user:     process.env.DB_USER         || "root",
+  password: process.env.DB_PASSWORD     || "",
+  database: process.env.DB_NAME         || "wayback",
+  port:     process.env.DB_PORT         || 3306
 });
 
 connection.connect((err) => {
