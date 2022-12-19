a0734bd9af92d962a7cb1cee1a268ef4dd4bd9f2
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-19 23:53:24 +0100

    db-host link fixed

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "containers- us - west - 161.railway.app",
+  host: process.env.DB_HOST || "containers-us-west-161.railway.app",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "EjpHf7yEAR7BsJ08xoEY",
   database: process.env.DB_NAME || "railway",
