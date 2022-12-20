9f0576a90c8428875d864c5b768b51276eba3166
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-20 01:39:19 +0100

    try

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.set("port", process.env.PORT || 3000)
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(locationRouters)
-// app.use(bookRouters)
+app.use(locationRouters);
+
 app.use((req, res, next) => {
     res.status(404).json({
         error: true,
