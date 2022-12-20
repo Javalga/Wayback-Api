1adf7d9dff2fabbc363caf47d0cdf2fa48a01748
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-20 14:21:02 +0100

    solve

--- src/apiRest.js
+++ src/apiRest.js
@@ -2,4 +2,4 @@ const app = require('./app')
 
 app.listen(process.env.PORT || 3000)
 
-console.log(`Servidor escuchando en el puerto ${app.get('port')}`);
+// console.log(`Servidor escuchando en el puerto ${app.get('port')}`);
