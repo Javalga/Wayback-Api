462b1ee80fb8aae70757e91859a68b3223f9cc86
Javier Alonso <jalonsogal@gmail.com>
2022-12-20 23:21:53 +0100

    e

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -1,7 +1,6 @@
 const connection = require("../database");
 
 function getUsers(request, response) {
-  
   let sql = "SELECT u.username, u.password, u.name, r.name AS role, u.mail, w.name AS warehouse, l.name AS location, u.active From users AS u INNER JOIN roles AS r ON(u.role_id = r.role_id)  INNER JOIN warehouses AS w ON(u.warehouse_id = w.warehouse_id) INNER JOIN locations AS l ON(l.location_id = u.location_id);";
   connection.query(sql, function (err, result) {
     if (err) {
@@ -13,4 +12,15 @@ function getUsers(request, response) {
   });
 }
 
-module.exports = { getUsers };
+const login = (req, res) => {
+  let sql = `SELECT username, password FROM users WHERE username = ${req.body.username} AND password = ${req.body.password}`;
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.send(result);
+      console.log(result);
+    }
+  });
+}
+module.exports = { getUsers, login };
--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -3,5 +3,6 @@ const router = Router();
 const usersCtrl = require('../controller/users.controller')
 
 router.get("/users", usersCtrl.getUsers);
+router.post("/login", usersCtrl.login);
 
 module.exports = router;
