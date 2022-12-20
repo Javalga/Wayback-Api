70bf5587ff9b8c7d6bc4f13f0079a58236f78647
Javier Alonso <jalonsogal@gmail.com>
2022-12-20 02:00:29 +0100

    a

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 const express = require("express")
 const cors = require("cors")
 const locationRouters = require('./routers/locations.router')
-
 const errorHandling = require('./error/errorHandling')
 
 const app = express()
 
-app.set("port", process.env.PORT || 3000)
+app.set("port", process.env.PORT || 7455)
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(locationRouters);
-
 app.use((req, res, next) => {
     res.status(404).json({
         error: true,
--- src/controller/locations.controller.js
+++ src/controller/locations.controller.js
@@ -1,7 +1,7 @@
 const connection = require("../database");
 
 function getLocations(request, response) {
-  
+  console.log('eo');
   let sql = "SELECT * FROM locations;";
   connection.query(sql, function (err, result) {
     if (err) {
@@ -10,7 +10,7 @@ function getLocations(request, response) {
       response.send(result);
       console.log(result);
     }
-  }); 
+  });
 }
 
-module.exports = { getLocations };
+module.exports = getLocations;
