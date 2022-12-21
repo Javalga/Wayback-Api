2a52e14274f9b1e31149862882fca11c1132bcc2
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-21 10:19:43 +0100

    m

--- src/controller/warehouses.controller.js
+++ src/controller/warehouses.controller.js
@@ -2,7 +2,7 @@ const connection = require("../database");
 
 function getWarehouses(request, response) {
   
-  let sql = "SELECT w.warehouse_id ,w.name, l.name AS location FROM warehouses AS w INNER JOIN locations AS l ON(l.location_id = w.location_id);";
+  let sql = "SELECT w.warehouse_id ,w.name, l.name AS location, w.location_id FROM warehouses AS w INNER JOIN locations AS l ON(l.location_id = w.location_id);";
   connection.query(sql, function (err, result) {
     if (err) {
       console.log(err);
@@ -13,4 +13,19 @@ function getWarehouses(request, response) {
   });
 }
 
-module.exports = { getWarehouses };
+function postWarehouses(request, response){
+
+  let params = [];
+  let sql =
+    "SELECT w.warehouse_id ,w.name, l.name AS location FROM warehouses AS w INNER JOIN locations AS l ON(l.location_id = w.location_id);";
+  connection.query(sql,params, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.send(result);
+      console.log(result);
+    }
+  });
+}
+
+module.exports = { getWarehouses, postWarehouses };
