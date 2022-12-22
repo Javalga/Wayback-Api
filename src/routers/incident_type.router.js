cce86bd35e65baceeaec74abe68fabe6ee34d8fe
Javier Alonso <jalonsogal@gmail.com>
2022-12-22 20:27:53 +0100

    a

--- src/routers/incident_type.router.js
+++ src/routers/incident_type.router.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
-const incident
-router.get("/incident_type", incidence_typeCtrl.getIncidence_type);
+const incident_typeCtrl = require('../controller/incidence_type.controller')
+
+router.get("/incident_type", incident_typeCtrl.getIncidence_type);
 
 module.exports = router;
