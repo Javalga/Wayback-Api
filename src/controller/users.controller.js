90a779daf7d96dce9d3bfe1bfc000c941e90b703
Javier Fernández Jiménez <javifav92@gmail.com>
2022-12-22 19:48:35 +0100

    a

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -1,7 +1,7 @@
 const connection = require("../database");
 
 function getUsers(request, response) {
-  let sql = "SELECT u.username, u.password, u.name, r.name AS role, u.mail, w.name AS warehouse, l.name AS location, u.active From users AS u INNER JOIN roles AS r ON(u.role_id = r.role_id)  INNER JOIN warehouses AS w ON(u.warehouse_id = w.warehouse_id) INNER JOIN locations AS l ON(l.location_id = u.location_id);";
+  let sql = "SELECT u.username, u.password, u.name, r.name AS role, u.mail, w.name AS warehouse, l.name AS location,u.warehouse_id,u.location_id,u.role_id, u.active From users AS u INNER JOIN roles AS r ON(u.role_id = r.role_id)  INNER JOIN warehouses AS w ON(u.warehouse_id = w.warehouse_id) INNER JOIN locations AS l ON(l.location_id = u.location_id);";
   connection.query(sql, function (err, result) {
     if (err) {
       console.log(err);
