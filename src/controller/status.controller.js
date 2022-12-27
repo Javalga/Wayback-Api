5f7c92658e88fc6e324b88fc173f4f4e9b7f1d66
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-27 21:51:55 +0100

    incidence-output finished

--- src/controller/status.controller.js
+++ src/controller/status.controller.js
@@ -12,15 +12,16 @@ const getStatus = (request, response) => {
     });
 };
 
-const putStatus = (request, response) => {
+const putStatusIncidenceOutpu = (request, response) => {
     console.log(request.body)
     let params = [
       request.body.status_id,
+      request.body.date,
       request.body.incidence_ref,
       request.body.warehouse_id,
     ];
   let sql =
-    "UPDATE `incidence` SET `status_id` = ? WHERE (`incidence_ref` = ? AND `warehouse_id` = ?);";
+    "UPDATE `incidence` SET `status_id` = ?, `output_date` = ? WHERE (`incidence_ref` = ? AND `warehouse_id` = ?);";
   connection.query(sql, params, function (err, result) {
     if (err) {
       console.log(err);
@@ -33,4 +34,4 @@ const putStatus = (request, response) => {
 
 
 
-module.exports = { getStatus, putStatus};
+module.exports = { getStatus, putStatusIncidenceOutpu };
--- src/routers/status.router.js
+++ src/routers/status.router.js
@@ -3,6 +3,6 @@ const router = Router();
 const statusCtrl = require("../controller/status.controller");
 
 router.get("/status", statusCtrl.getStatus);
-router.put("/status", statusCtrl.putStatus);
+router.put("/status", statusCtrl.putStatusIncidenceOutpu);
 
 module.exports = router;
