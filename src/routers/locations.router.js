3eeb2542f2ce9d144b414a83eeca6d41ce7175f0
Javier Fernández Jiménez <javifav92@gmail.com>
2023-01-09 22:00:28 +0100

    locations

--- src/routers/locations.router.js
+++ src/routers/locations.router.js
@@ -3,5 +3,6 @@ const router = Router();
 const locationsCtrl = require('../controller/locations.controller')
 
 router.get('/locations', locationsCtrl.getLocations)
+router.post('/locations', locationsCtrl.postLocations)
 
 module.exports = router;
