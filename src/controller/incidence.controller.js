2d30f77870587ea58df631f5073ecc73d90c08e7
alanheredero11@gmail.com <alanheredero11@gmail.com>
2023-01-09 20:12:04 +0100

    alan

--- src/controller/incidence.controller.js
+++ src/controller/incidence.controller.js
@@ -157,7 +157,7 @@ const putIncidence = (req, res) => {
     customer_city = \"${incidence.customer_city}\", 
     next_delivery= \"${incidence.next_delivery}\",
     delivery_time_id= \"${incidence.delivery_time_id}\"
-    WhERE incidence_ref = \"${incidence.incidence_ref}\"`
+    WHERE incidence_ref = \"${incidence.incidence_ref}\"`
     answer = { error: false, code: 200, message: 'Incidence updated', result: mark }
   } else {
     console.log('Please fill all the inputs');
--- src/routers/incidence.router.js
+++ src/routers/incidence.router.js
@@ -7,6 +7,7 @@ router.get("/incidence_processed", incidentsCtrl.getIncidenceProcessed);
 router.post("/incidence", incidentsCtrl.postIncidence);
 router.get("/incidence_solved", incidentsCtrl.getIncidenceSolved);
 router.get("/incidence_to_return", incidentsCtrl.getIncidenceToReturn);
+router.get("/incidence_form", incidentsCtrl.putIncidence);
 
 
 
