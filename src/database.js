b8666e25f155bec9bc79efed5655eb0d50d5f138
alanheredero11@gmail.com <alanheredero11@gmail.com>
2023-01-16 11:01:48 +0100

    m

--- src/database.js
+++ src/database.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "containers-us-west-161.railway.app",
-  user: process.env.DB_USER || "root",
+  host:
+    process.env.DB_HOST ||
+    "wayback-database.cyavsibz6fni.us-east-1.rds.amazonaws.com",
+  user: process.env.DB_USER || "admin",
   password: process.env.DB_PASSWORD || "EjpHf7yEAR7BsJ08xoEY",
-  database: process.env.DB_NAME || "railway",
-  port: process.env.DB_PORT || 7455
+  database: process.env.DB_NAME || "",
+  port: process.env.DB_PORT || 3306,
 });
 
 connection.connect((err) => {
